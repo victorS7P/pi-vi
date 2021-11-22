e34abdddc0520da987b284e623f29364f4bef757
victorS7P <victorsilva7p@gmail.com>
2021-11-22 12:59:17 -0300

    feat: add connection between scrappers and database

--- scrappers/src/main.py
+++ scrappers/src/main.py
@@ -1,6 +1,16 @@
+import os
 import time
+from dotenv import load_dotenv
+
+from pymongo import MongoClient
 
 if __name__ == '__main__':
+  load_dotenv()
+  mongo_url = os.environ['DB_URL']
+  client = MongoClient(mongo_url)
+
+  db = client[os.environ['DB_NAME']]
+
   while(True):
-    print('SCRAPPERS OK!')
-    time.sleep(1)
+    db.names.insert_one({ "lorem": "ipsum" })
+    time.sleep(5)
